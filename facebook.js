02e927c4058b77144857633aad161ad2f7064a06
Lucifer <29118942+LUCIFER3944@users.noreply.github.com>
2023-10-10 10:07:09 +0530

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,14 @@
+
+alert("Click on all buttons ")
+
+function greet(){
+    alert("You are Successfully Login  ✅ .")
+}
+
+function forgot(){
+    alert("Sorry I can not Help You.")
+}
+
+function create(){
+    alert("Account Created")
+}
